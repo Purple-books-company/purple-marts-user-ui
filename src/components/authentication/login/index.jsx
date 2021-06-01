1d63ae16f94f7737c915f4ca93f5c9da7f982b2c
Shalini-CEG <ahana298@gmail.com>
2021-06-01 21:51:57 +0530

    Button Widget Test

--- src/components/authentication/login/index.jsx
+++ src/components/authentication/login/index.jsx
@@ -1,5 +1,12 @@
+import { Button } from "../../../styles/widgets";
 const Login = () => {
-  return <div>Login Page</div>;
+  return (
+    <div>
+      Login Page
+      <br />
+      <Button>Login</Button>
+    </div>
+  );
 };
 
 export default Login;
