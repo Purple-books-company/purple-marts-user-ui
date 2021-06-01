532838289e9de4f5629d66f4e2f4937302e84ece
Shalini-CEG <ahana298@gmail.com>
2021-06-01 21:52:12 +0530

    Enhanced styling

--- src/styles/widgets.js
+++ src/styles/widgets.js
@@ -1,20 +1,23 @@
 import styled, { css } from "styled-components";
 
 // Contains widgets like Button, Text box etc.
-
+// Darker color code: #8c49ad
 export const Button = styled.button`
-  background: transparent;
-  border-radius: 20px;
+  background: #b097bd;
+  border-radius: 5px;
   border: 2px solid #b097bd;
-  color: #b097bd;
+  color: white;
   margin: 0 1em;
   font-family: Helvetica, sans-serif;
-  width: 100px;
-  height: 30px;
+  font-size: 15px;
+  font-weight: bold;
+  width: 150px;
+  height: 50px;
+  margin-top: 40px;
   padding: 0.25em 1em;
 
   &:hover {
-    background: #8c49ad;
-    color: white;
+    background: transparent;
+    color: #b097bd;
   }
 `;
