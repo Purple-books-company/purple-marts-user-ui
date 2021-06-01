c006fa73cbdfa274477cacff345884bfe97d9579
Shalini-CEG <ahana298@gmail.com>
2021-06-01 21:32:31 +0530

    Added primary routes

--- src/navigations/Routes.jsx
+++ src/navigations/Routes.jsx
@@ -1,7 +1,26 @@
+import Login from "../components/authentication/login";
+import Register from "../components/authentication/register";
+import Home from "../components/pages/home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 function Routes() {
   return (
     <div>
-      <h3>Navigations</h3>
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/register">
+              <Register />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
