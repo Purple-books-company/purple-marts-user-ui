7f1dcc105cd612f4729117de6185f3035b718e5f
Shalini-CEG <ahana298@gmail.com>
2021-06-03 20:45:25 +0530

    Trending Products slider

---
+++ src/components/pages/home/TrendingProducts.jsx
@@ -0,0 +1,39 @@
+import Slider from "react-slick";
+import { Block, Image, Text } from "../../../styles/home-page";
+import data from "../../../api/TrendingProducts.json";
+import { useEffect, useState } from "react";
+
+export default function TrendingProducts() {
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+  };
+
+  const [urls, setUrls] = useState([]);
+
+  useEffect(() => {
+    setUrls(data);
+  }, []);
+
+  return (
+    <>
+      <center>
+        <Text primary>TRENDING PRODUCTS ARE HERE</Text>
+        <div className="container" style={{ marginTop: "30px" }}>
+          <div className="row">
+            <Slider {...settings}>
+              {urls.map((url) => (
+                <Block primary className="col mt-2" key={url}>
+                  <Image src={url.PrdtImg} alt="Phone cases" />
+                </Block>
+              ))}
+            </Slider>
+          </div>
+        </div>
+      </center>
+    </>
+  );
+}
