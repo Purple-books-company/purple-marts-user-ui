c02d870c5dab39e73b54f028ab7fe4be307fe4d7
Shalini-CEG <ahana298@gmail.com>
2021-06-04 10:00:04 +0530

    Optimised code

--- src/components/pages/home/components/Grid.jsx
+++ src/components/pages/home/components/Grid.jsx
@@ -2,42 +2,32 @@ import { DarkShade } from "../../../../styles/color-theme";
 import { Block, Caption } from "../../../../styles/home-page";
 
 const Grid = () => {
+  const gridImg = [
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTugCX5kSC9m1XFVcK7BWoZY9HiycolFcoTQ&usqp=CAU",
+    "https://m.media-amazon.com/images/I/61R4wTQfJdL._AC_SX425_.jpg",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRu5mYIH9y9ovhGwpiqquVjk2g6ivCHrnSO1g&usqp=CAU",
+  ];
+
+  const gridText = ["PHONE ACCESSORIES", "SHOP ESSENTIALS", "SHOP HEAD PHONES"];
   return (
     <>
       <div className="container">
         <div className="row">
-          <div className="col-md-3 mt-4">
-            <Block
-              style={{
-                backgroundImage: `url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTugCX5kSC9m1XFVcK7BWoZY9HiycolFcoTQ&usqp=CAU")`,
-              }}
-            >
-              <Caption> PHONE ACCESSORIES</Caption>
-            </Block>
-          </div>
-          <div className="col-md-3 mt-4">
-            <Block
-              style={{
-                backgroundImage: `url("https://m.media-amazon.com/images/I/61R4wTQfJdL._AC_SX425_.jpg")`,
-              }}
-            >
-              <Caption>HAPPY SHOP</Caption>
-            </Block>
-          </div>
+          {gridImg.map((img, index) => (
+            <div className="col-md-3 mt-4">
+              <Block
+                style={{
+                  backgroundImage: `url(${img})`,
+                }}
+              >
+                <Caption>{gridText[index]}</Caption>
+              </Block>
+            </div>
+          ))}
 
           <div className="col-md-3 mt-4">
             <Block style={{ background: DarkShade }}>
-              <Caption>SHOP HEAD PHONES</Caption>
-            </Block>
-          </div>
-
-          <div className="col-md-3 mt-4">
-            <Block
-              style={{
-                backgroundImage: `url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRu5mYIH9y9ovhGwpiqquVjk2g6ivCHrnSO1g&usqp=CAU")`,
-              }}
-            >
-              <Caption>HAPPY SHOP</Caption>
+              <Caption>SHOP AIR PODS</Caption>
             </Block>
           </div>
         </div>
