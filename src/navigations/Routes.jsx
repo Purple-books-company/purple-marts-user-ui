a029cfae0437d6dabebd5ab5e2f11dd234aaeb29
Shalini-CEG <ahana298@gmail.com>
2021-06-07 20:58:36 +0530

    Routes changes

--- src/navigations/Routes.jsx
+++ src/navigations/Routes.jsx
@@ -1,26 +1,32 @@
-import Login from "../components/authentication/login";
-import Register from "../components/authentication/register";
+import { useState } from "react";
+import LogOut from "../components/pages/authentication/components/LogOut";
 import Home from "../components/pages/home";
 import Cart from "../components/pages/cart";
 import Category from "../components/pages/Category";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Search from "../components/common/Search";
+import Search from "../components/utils/Search";
+import Wrapper from "../components/pages/authentication/";
 
 function Routes() {
+  const [showModal, setShowModal] = useState(false);
+
+  const openModal = () => {
+    setShowModal((prev) => !prev);
+  };
   return (
     <div>
       <Search />
+      <button onClick={openModal}>Login</button>
+      <LogOut />
+      <Wrapper showModal={showModal} setShowModal={setShowModal} />
       <Router>
         <div>
           <Switch>
             <Route exact path="/">
               <Home />
             </Route>
-            <Route path="/register">
-              <Register />
-            </Route>
-            <Route path="/login">
-              <Login />
+            <Route path="/auth/login">
+              <Wrapper />
             </Route>
             <Route path="/category">
               <Category />
