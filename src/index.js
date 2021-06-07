8b9234c5fd5881d2d5496452c2019a1c30830741
Shalini-CEG <ahana298@gmail.com>
2021-06-07 20:55:57 +0530

    Bootstrap Links

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import $ from "jquery";
+import Popper from "popper.js";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 import Routes from "./navigations/Routes";
 
 ReactDOM.render(
@@ -8,5 +12,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-
