bfb6820874bee8e2128b5d455e2be5bc7d671ff0
Shalini-CEG <ahana298@gmail.com>
2021-06-08 18:23:39 +0530

    Commented unused imports

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import $ from "jquery";
-import Popper from "popper.js";
+// import $ from "jquery";
+// import Popper from "popper.js";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import Routes from "./navigations/Routes";
 
