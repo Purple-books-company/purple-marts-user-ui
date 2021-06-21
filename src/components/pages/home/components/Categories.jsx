5c09659087b909307d03321ad31894470470e42f
Shalini-CEG <ahana298@gmail.com>
2021-06-21 11:39:33 +0530

    Fetched dynamically

--- src/components/pages/home/components/Categories.jsx
+++ src/components/pages/home/components/Categories.jsx
@@ -1,55 +1,57 @@
 import { useEffect, useState } from "react";
 import Slider from "react-slick";
+import { fetchResult } from "../../../../services/api/get-services";
 import { Block, Image } from "../../../../styles/pages/home-page";
 import { Text } from "../../../../styles/widgets/widgets";
-import { ApiGetService } from "../../../../services/api/api-services";
 
 export default function Categories() {
   const [urls, setUrls] = useState([]);
-
-  let slides;
-  if (urls.length >= 5) slides = 5;
-  else slides = urls.length % 5;
-
-  let settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: slides,
-    slidesToScroll: 1,
-    swipeToSlide: true,
-  };
+  const [settings, setSettings] = useState(null);
 
   const fetchCategories = async () => {
+    let slides;
     let categories = [];
-    categories = await ApiGetService(process.env.REACT_APP_CATEGORY_GET_URL);
+    categories = await fetchResult("categories");
     if (categories === null) categories = [];
     setUrls(categories);
+
+    if (categories.length >= 5) slides = 5;
+    else slides = categories.length % 5;
+
+    setSettings({
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: slides,
+      slidesToScroll: 1,
+      swipeToSlide: true,
+    });
   };
 
   useEffect(() => {
     fetchCategories();
-  }, [urls]);
+  }, []);
 
   return (
     <>
       <center>
-        <Text primary>CATEGORIES TO BAG </Text>
+        <Text primary>CATEGORIES TO BAG</Text>
         <div className="container" style={{ marginTop: "10px" }}>
           <div className="row">
             <Slider {...settings}>
-              {urls.map((url) => (
-                <Block
-                  primary
-                  className="col mt-2"
-                  key={url}
-                  data-bs-toggle="tooltip"
-                  data-bs-placement="right"
-                  title={url.name}
-                >
-                  <Image src={url.image} alt="Category" />
-                </Block>
-              ))}
+              {urls.length > 0 &&
+                urls.map((url) => (
+                  <Block
+                    primary
+                    className="col mt-2"
+                    key={url}
+                    data-bs-toggle="tooltip"
+                    data-bs-placement="right"
+                    title={url.name}
+                  >
+                    <Image src={url.image} alt="Category" />
+                  </Block>
+                ))}
             </Slider>
           </div>
         </div>
