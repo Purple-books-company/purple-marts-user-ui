ae0c48402045d32ffc836fbb46b3f347863c2001
Yuvaraj2111 <64225422+Yuvaraj2111@users.noreply.github.com>
2021-06-30 02:55:59 +0530

    Nav bar collapse addedd

--- src/styles/pages/header.js
+++ src/styles/pages/header.js
@@ -1,25 +1,19 @@
-import { NavLink as Link } from "react-router-dom";
 import styled, { css } from "styled-components";
-import { DarkShade, LightShade, Silver } from "../themes/color-theme";
-import { FcMenu } from "react-icons/fc";
-import { Lora, Slab } from "../themes/font-styles";
-import { Image, Navbar, Nav } from "react-bootstrap";
+import { DarkShade, LightShade } from "../themes/color-theme";
+import { Navbar, Nav } from "react-bootstrap";
 
 // Nav bar styles
 export const HeaderNav = styled(Navbar)`
-  /* padding: 0.5%; */
-  background-color: white;
   border-bottom: 0.1rem solid ${LightShade};
-  height: 10%;
-  @media (min-width: 0px) and (max-width: 1000px) {
-    display: none;
-  }
 `;
 
 export const BrandImg = styled.img`
-  /* margin-top: 1%; */
-  margin-left: 20%;
-  height: 3.2rem;
+  margin-left: -5em;
+  height: 3.5rem;
+  @media(min-width:0) and (max-width:800px){
+    height:3rem;
+    margin-left: 0em;
+  }
 `;
 
 export const CartNum = styled.p`
@@ -36,11 +30,12 @@ export const NavLink = styled(Nav.Link)`
   font-weight: 600;
   font-size: 88%;
   letter-spacing: 0.2vh;
-  margin: 0 0.7rem;
+  margin: 0 0.7rem 1em 0.7rem;
   color: #535252 !important;
-  border-bottom: 0.2rem solid white;
   cursor: pointer;
   text-transform: uppercase;
+  border-bottom: 0.2rem solid;
+  border-bottom-color: white;
   ${(props) =>
     props.icon &&
     css`
@@ -51,108 +46,24 @@ export const NavLink = styled(Nav.Link)`
       text-transform: capitalize;
       margin-left: 2%;
       margin-right: 1%;
+      @media (min-width: 0px) and (max-width: 1000px){
+        text-align: left;
+        align-items: left;
+        font-size: 88% !important;
+        font-weight: 600;
+        margin: 0 0.7rem;
+        text-transform: uppercase;
+      }
     `}
-
+    @media (min-width: 1000px){
   :hover {
     color: ${DarkShade} !important;
     ${(props) =>
-      props.color &&
-      css`
+    props.color &&
+    css`
         border-bottom: 0.2rem solid;
         border-bottom-color: ${props.color};
       `}
   }
-`;
-
-// Slider styles
-
-// logo styles
-export const LogoImg = styled(Image)`
-  margin: 0;
-  height: 10%;
-  width: 15%;
-  @media (min-width: 0px) and (max-width: 568px) {
-    height: 35%;
-    width: 30%;
-  }
-`;
-
-export const LogoBox = styled.div`
-  display: flex;
-  justify-content: center;
-  align-content: center;
-  @media (min-width: 1000px) {
-    display: none;
-  }
-`;
-
-export const Menu = styled(FcMenu)`
-  display: none;
-  @media (min-width: 0px) and (max-width: 1000px) {
-    display: flex;
-    margin-left: 2px;
-  }
-`;
-
-export const SliderBtn = styled.p`
-  @media (min-width: 0px) and (max-width: 1000px) {
-    display: flex;
-  }
-  @media (min-width: 1000px) {
-    display: none;
-  }
-`;
-
-export const CanvasHeader = styled.div`
-  background-color: #ffa1ed;
-`;
-
-export const ProfileImg = styled.img`
-  margin: 20px;
-  object-fit: contain;
-  height: 60px;
-  width: 60px;
-  border-width: 2px;
-  border-radius: 10px;
-  border-color: red;
-`;
-
-export const ProfileName = styled(Link)`
-  margin-left: 22px;
-  font-weight: bold;
-  font-family: ${Lora};
-  text-decoration: none;
-  color: black;
-  :hover {
-    color: white;
-  }
-`;
-
-export const CanvasBody = styled.div``;
-
-export const NavBtn = styled(Link)`
-  text-decoration: none;
-  border-bottom-width: 1px;
-  border-bottom-color: gray;
-  text-transform: uppercase;
-  text-align: center;
-  font-size: medium;
-  font-family: ${Slab};
-  width: 100%;
-  margin: 5px;
-  color: ${DarkShade};
-  :hover {
-    color: white;
-  }
-`;
-
-export const SlideBtnsDiv = styled.div`
-  padding: 15px;
-  border-width: 1px;
-  border-color: gray;
-  color: ${DarkShade};
-  :hover {
-    background-color: ${LightShade};
-    color: ${Silver};
-  }
+    }
 `;
