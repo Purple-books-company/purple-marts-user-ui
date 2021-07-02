0dc61d7d764cf0595f4dc470fbe34f8d1f9f2735
Shalini-CEG <ahana298@gmail.com>
2021-07-02 21:37:15 +0530

    Home Page data updated

--- src/components/pages/home/index.jsx
+++ src/components/pages/home/index.jsx
@@ -1,25 +1,37 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Grid from "./components/Grid";
 import Carousel from "./components/Carousel";
-import Categories from "./components/Categories";
-import { getApi } from "../../../services/api/loaded-services";
+import Sliders from "./components/Sliders";
+import Loading from "../../utils/loader";
+import { getApi, fetchResult } from "../../../services/api/loaded-services";
 
 export default function Home() {
+  const [home, setHome] = useState(null);
+
   useEffect(() => {
     getData();
   }, []);
 
   const getData = async () => {
     await getApi();
+    let values;
+    values = await fetchResult("home");
+    setHome({ ...values });
+    // console.log(values);
   };
 
   return (
     <>
-      <div>
-        <Carousel />
-        <Grid />
-        <Categories />
-      </div>
+      {home ? (
+        <div>
+          <Carousel data={home.carousel} />
+          <Grid data={home["Test Sale"]} />
+          <Sliders data={home.subCategory} text="EXPLORE TRENDING ITEMS" />
+          <Sliders data={home.category} text="CATEGORIES TO BAG" />
+        </div>
+      ) : (
+        <Loading />
+      )}
     </>
   );
 }
