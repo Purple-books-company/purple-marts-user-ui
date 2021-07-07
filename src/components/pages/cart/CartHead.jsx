27d275d58bb1abeb559af4a60d7a5e239049066b
Yuvaraj2111 <64225422+Yuvaraj2111@users.noreply.github.com>
2021-07-07 23:51:07 +0530

    api functionallity added for cart

--- src/components/pages/cart/CartHead.jsx
+++ src/components/pages/cart/CartHead.jsx
@@ -1,12 +1,13 @@
 import { BsArrowLeftShort } from "react-icons/bs";
 import { ContinueBtn, CartTitle } from "../../../styles/pages/cart-page";
-
+import { useHistory } from "react-router";
 const CartHead = () => {
+  let history = useHistory()
   return (
     <>
       <div className="row container">
         <CartTitle className="col">My Cart</CartTitle>
-        <ContinueBtn className="" aln="true">
+        <ContinueBtn className="" aln="true" onClick={() => history.push('/')}>
           <span className="">
             <BsArrowLeftShort className="mx-2" />
             Continue shopping
